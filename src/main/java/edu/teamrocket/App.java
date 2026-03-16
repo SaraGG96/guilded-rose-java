1aa9f000a719d711e625f1d7182a4d66ff7f2ccd
Sara <saragarciagarrido96@gmail.com>
2026-03-16 13:13:24 +0100

    first commit

---
+++ src/main/java/edu/teamrocket/App.java
@@ -0,0 +1,7 @@
+package edu.teamrocket;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/edu/teamrocket/Item.java
@@ -0,0 +1,14 @@
+package edu.teamrocket;
+
+public class Item {
+
+    private final String name;
+    private int sell_in;
+    private int quality;
+
+    //constructor
+    Item(String, int, int) {
+        
+    };
+
+}
