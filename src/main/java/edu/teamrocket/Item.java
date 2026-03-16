43eeb5bff5c39a5cb920c67e925a40c927b6a082
Sara <saragarciagarrido96@gmail.com>
2026-03-16 13:25:41 +0100

    feat(Item.java): Inicializar clase Item

--- src/main/java/edu/teamrocket/Item.java
+++ src/main/java/edu/teamrocket/Item.java
@@ -2,13 +2,41 @@ package edu.teamrocket;
 
 public class Item {
 
+    //atributos
     private final String name;
     private int sell_in;
     private int quality;
 
     //constructor
-    Item(String, int, int) {
-        
+    Item(String name, int sell_in, int quality) {
+        this.name = name;
+        this.sell_in = sell_in;
+        this.quality = quality;
     };
+
+    //métodos
+     String getName() {
+        return name;
+    }
+
+    int getSell_in() {
+        return sell_in;
+    }
+
+    int getQuality() {
+        return quality;
+    }
+
+    void setSell_in(int sell_in) {
+        this.sell_in = sell_in;
+    }
+
+    void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {};
+
 
 }
